6618c74047a5fa182eaf1ab54b531f823d1910f4
hsea3009 <hsea300@gmail.com>
2025-12-29 12:34:44 -0500

    Fix file path string in get_book_text function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
   def get_book_text():
-    with open(books/frankenstein.txt) as f:
+    with open("books/frankenstein.txt") as f:
       file_contexts = f.read()
 
 # if __name__ == "__main__":
