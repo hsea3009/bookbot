b504012a026e220d89d5824891a978da644fda2e
hsea3009 <hsea300@gmail.com>
2025-12-29 12:29:34 -0500

    Fix indentation for get_book_text function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
-def get_book_text():
-  with open(books/frankenstein.txt) as f:
-    file_contexts = f.read()
+  def get_book_text():
+    with open(books/frankenstein.txt) as f:
+      file_contexts = f.read()
 
 if __name__ == "__main__":
   main()
