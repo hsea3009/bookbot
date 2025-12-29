c7f41e9b73680fecf517a82d839742771025df23
hsea3009 <hsea300@gmail.com>
2025-12-29 18:11:58 -0500

    Add word_count function to count words in a file

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ def get_book_text(file_path):
     file_contexts = f.read()
     return file_contexts
 
+def word_count(file_path):
+  with open(file_path) as f:
+    file_contexts = f.read()
+    words = file_contexts.split()
+    return words
+
 def main():
   book_text = get_book_text("books/frankenstein.txt")
   print(book_text)
+  words = word_count("books/frankenstein.txt")
+  print(words)
+
+  
 
 if __name__ == "__main__":
   main()
