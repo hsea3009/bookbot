c473153f0612fb8cb6006ceef997988f89f8e140
hsea3009 <hsea300@gmail.com>
2025-12-29 18:26:55 -0500

    Remove word_count function
    Removed the word_count function from main.py.

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ def get_book_text(file_path):
     file_contexts = f.read()
     return file_contexts
 
-def word_count(file_path):
-  with open(file_path) as f:
-    file_contexts = f.read()
-    words = file_contexts.split()
-    return len(words)
 
 def main():
   book_text = get_book_text("books/frankenstein.txt")
