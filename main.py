9b3f770d8941563c32c6b61558eaa03a88bcb2e5
hsea3009 <hsea300@gmail.com>
2025-12-29 12:28:57 -0500

    Fix main function call in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def main():
 def get_book_text():
   with open(books/frankenstein.txt) as f:
     file_contexts = f.read()
 
-# if __name__ == "__main__":
-main()
+if __name__ == "__main__":
+  main()
