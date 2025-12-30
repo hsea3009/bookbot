5b583d321bf31123ef3e6dbbb4ddfeb7008a195d
hsea3009 <hsea300@gmail.com>
2025-12-29 20:09:52 -0500

    Implement text_count function to display file contents
    Added text_count function to read and print file contents.

--- stats.py
+++ stats.py
@@ -3,3 +3,10 @@ def word_count(file_path):
     file_contexts = f.read()
     words = file_contexts.split()
     return len(words)
+
+def text_count(file_path):
+  with open(file_path) as f:
+    file_contexts = f.read()
+    print(file_contexts)
+
+text_count("books/frankenstein.txt")
