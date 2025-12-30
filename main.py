585a84f1a9675f85f5125a6e486ee482d08ecac6
hsea3009 <hsea300@gmail.com>
2025-12-29 20:21:42 -0500

    Import text_count and print total text count
    Added text_count function to count total text in the book.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, text_count
 
 def get_book_text(file_path):
   with open(file_path) as f:
@@ -12,7 +12,8 @@ def main():
   words = word_count("books/frankenstein.txt")
   print(f'Found {words} total words')
 
-  
+  text = text_count("books/frankenstein.txt")
+  print(text)
 
 if __name__ == "__main__":
   main()
