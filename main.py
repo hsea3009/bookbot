1643cd0e5151a2190037aa4c54bb2a3a41a42cf3
hsea3009 <hsea300@gmail.com>
2025-12-30 21:44:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
-from stats import word_count,chars_dict_to_sorted_list, get_char_dict
+# from stats import word_count,chars_dict_to_sorted_list, get_char_dict
+
+
+# def get_book_text(file_path):
+#   with open(file_path) as f:
+#     file_contexts = f.read()
+#     return file_contexts
+
+
+# def main():
+#   text = get_book_text("books/frankenstein.txt")
+#   num_words = word_count(text)
+#   chars_dict = get_char_dict(text)               
+#   sorted_chars = chars_dict_to_sorted_list(chars_dict)
+
+#   for item in sorted_chars:
+#     ch = item["char"]
+#     if not ch.isalpha():
+#       continue
+#   print(f"{ch}: {item['num']}")
+
+# if __name__ == "__main__":
+#   main()
+
+
+from stats import word_count, chars_dict_to_sorted_list, get_char_dict
 
 
 def get_book_text(file_path):
-  with open(file_path) as f:
-    file_contexts = f.read()
-    return file_contexts
+    with open(file_path) as f:
+        return f.read()
 
 
 def main():
-  text = get_book_text("books/frankenstein.txt")
-  num_words = word_count(text)
-  chars_dict = get_char_dict(text)               
-  sorted_chars = chars_dict_to_sorted_list(chars_dict)
+    text = get_book_text("books/frankenstein.txt")
 
-  for item in sorted_chars:
-    ch = item["char"]
-    if not ch.isalpha():
-      continue
-  print(f"{ch}: {item['num']}")
+    # word count
+    num_words = word_count(text)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    # char counts
+    chars_dict = get_char_dict(text)
+    sorted_chars = chars_dict_to_sorted_list(chars_dict)
+
+    for item in sorted_chars:
+        ch = item["char"]
+        if not ch.isalpha():
+            continue
+        print(f"{ch}: {item['num']}")
+    print("============= END ===============")
+
 
 if __name__ == "__main__":
-  main()
-
+    main()
