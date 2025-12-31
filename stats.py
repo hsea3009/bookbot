b3f88fe3db79121bee8b9b7cc4681fffb75bf601
hsea3009 <hsea300@gmail.com>
2025-12-30 21:41:48 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,8 +1,6 @@
-def word_count(file_path):
-  with open(file_path) as f:
-    file_contexts = f.read()
-    words = file_contexts.split()
-    return len(words)
+def word_count(text):
+  words = text.split()
+  return len(words)
 
 def text_count(file_path):
   p_count = 0
