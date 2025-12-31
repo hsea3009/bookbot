d909b8fd399cd15d4ee14f2c77a7aabd88a12cdf
hsea3009 <hsea300@gmail.com>
2025-12-30 21:43:29 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,16 +1,50 @@
+# def word_count(text):
+#   words = text.split()
+#   return len(words)
+
+# def get_char_dict(text):
+#     letter_counts = {}
+#     for char in text.lower():
+#       if char.isalpha():
+#         if char in letter_counts:
+#           letter_counts[char] += 1
+#         else:
+#           letter_counts[char] = 1
+#     return letter_counts
+
+# def chars_dict_to_sorted_list(chars_dict):
+#     list_of_dict = []
+#     for char, num in chars_dict.items():
+#         entry = {"char": char, "num": num}
+#         list_of_dict.append(entry)
+
+#     def sort_on_num(item):
+#         return item["num"]
+
+#     list_of_dict.sort(reverse=True, key=sort_on_num)
+#     return list_of_dict
+
+
+
 def word_count(text):
-  words = text.split()
-  return len(words)
+    words = text.split()
+    return len(words)
+
 
 def get_char_dict(text):
     letter_counts = {}
     for char in text.lower():
-      if char.isalpha():
-        if char in letter_counts:
-          letter_counts[char] += 1
-        else:
-          letter_counts[char] = 1
+        if char.isalpha():
+            if char in letter_counts:
+                letter_counts[char] += 1
+            else:
+                letter_counts[char] = 1
     return letter_counts
+
+
+def sort_on(item):
+    return item["num"]
+
 
 def chars_dict_to_sorted_list(chars_dict):
     list_of_dict = []
@@ -18,8 +52,5 @@ def chars_dict_to_sorted_list(chars_dict):
         entry = {"char": char, "num": num}
         list_of_dict.append(entry)
 
-    def sort_on_num(item):
-        return item["num"]
-
-    list_of_dict.sort(reverse=True, key=sort_on_num)
+    list_of_dict.sort(reverse=True, key=sort_on)
     return list_of_dict
